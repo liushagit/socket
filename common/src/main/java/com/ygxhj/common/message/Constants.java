36d44c35e409e1da0468483ca12f5827bc7ead5a
ygxhj <674947119@qq.com>
2014-12-03 19:27:56 +0800

    init

--- common/src/main/java/com/ygxhj/common/message/Constants.java
+++ common/src/main/java/com/ygxhj/common/message/Constants.java
@@ -3,6 +3,6 @@ package com.ygxhj.common.message;
 public class Constants {
 
 
-	public static final String HOST = "192.168.1.243";
+	public static final String HOST = "192.168.1.241";
 	public static final int PORT = 8099;
 }
